2405a0109706e6b8e9631f29aea411ff09436395
chamodshehanka <hcsperera@outlook.com>
2023-12-18 17:49:06 +0530

    Dynamic Array Added

--- 1-Arrays/DynamicArray.java
+++ 1-Arrays/DynamicArray.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class DynamicArray {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        ArrayList<Integer> dynamicArray = new ArrayList<>();
+        int[] fixedArray = new int[5];
+
+        dynamicArray.add(10);
+        dynamicArray.add(20);
+        dynamicArray.add(30);
+        dynamicArray.add(40);
+        dynamicArray.add(40);
+
+        System.out.println("Dynamic Array : " + dynamicArray);
+
 
     }
 }
